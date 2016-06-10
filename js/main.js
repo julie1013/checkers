0c7dbf01dd5d99805610baea330fb35c540ba375
julie1013 <julie1380@verizon.net>
2016-06-09 21:29:42 -0400

    Checkers move now

--- js/main.js
+++ js/main.js
@@ -8,19 +8,132 @@ var checkerboard = [[null, null, null, null, null, null, null, null],
                     [null, null, null, null, null, null, null, null],
                     [null, null, null, null, null, null, null, null]];
 
-var redChecker;
-var blackChecker;
-var middleRow;
-var middleCol;
-var countR = 12;
-var countB = 12;
-var piece = 0;
-var redScoreCount = 0;
-var blackScoreCount = 0;
-var whoseTurn = 0;
-var jumped = false;
-var activePieceCoords = { row: -1,
-                          col: -1 };
+// var redChecker;
+// var blackChecker;
+// var countR = 12;
+// var countB = 12;
+// var piece = 0;
+// var redScoreCount = 0;
+// var blackScoreCount = 0;
+// var whoseTurn = 0;
+// var jumped = false;
+// var activePieceCoords = { row: -1,
+//                           col: -1 };
+
+
+var newBoard = new Board();
+
+function Rules(){
+
+  this.jumped = false;
+  this.whoseTurn = 0;
+
+
+  this.switchTurn = function() {
+    if (this.whoseTurn === 1) {
+      this.whoseTurn = 0;
+    } else {
+      this.whoseTurn = 1;
+    }
+    jumped = false;
+  };
+
+  this.resetMiddleRowCol = function(){
+    this.middleRow = null;
+    this.middleCol = null;
+  }
+
+  this.isKing = function(piece){
+    return (piece.hasClass("king"));
+  };
+  this.isRed = function(piece){
+    return (piece.hasClass("red"));
+  };
+
+  this.isBlack = function(piece){
+    return (piece.hasClass("black"));
+  };
+
+  this.isValidMove = function(firstRow, firstCol, endingRow, endingCol, piece){
+    if (!(gameRules.isValidSquare(firstRow, firstCol) && gameRules.isValidSquare(endingRow, endingCol))){
+        return false;
+    } else if (gameRules.isNextRow(firstRow, endingRow, piece) && gameRules.isAdjacentSpace(firstRow, firstCol, endingRow, endingCol)){
+        return true;
+    } else {
+        return false;
+    }
+  };
+
+  this.isValidJump = function(firstRow, firstCol, endingRow, endingCol, piece){
+    return (newBoard.isJumpToSquareOpen(firstRow, firstCol, endingRow, endingCol) && this.isOpponentOnJumpOverSquare(piece, firstRow, firstCol, endingRow, endingCol) && newBoard.isValidSquare(endingRow, endingCol) && newBoard.isTwoRows(firstRow, endingRow, piece));
+  };//new change
+
+  this.isJump = function(endingRow, firstRow){
+    return (Math.abs(endingRow - firstRow) === 2);
+  };
+
+  this.findMiddleRow = function(firstRow, endingRow){
+    if (firstRow + 1 === endingRow - 1) {
+        middleRow = firstRow + 1;
+    } else if (firstRow - 1 === endingRow + 1){
+        middleRow = firstRow - 1;
+    }
+  };
+
+  this.findMiddleCol = function(firstCol, endingCol){
+    if (firstCol + 1 === endingCol - 1){
+        middleCol = firstCol + 1;
+    } else if (firstCol - 1 === endingCol + 1){
+        middleCol = firstCol - 1;
+    }
+  };
+
+  this.isAdjacentSpace = function(firstRow, firstCol, endingRow, endingCol){
+    return Math.abs(endingCol - firstCol) === 1;
+  };
+
+  this.isTwoColumns = function(firstCol, endingCol){
+    return Math.abs(endingCol - firstCol) === 2;
+  };
+
+  this.isNextRow = function(firstRow, endingRow, piece){
+    if (!this.isKing(piece)){
+        if (this.isRed(piece)){
+            return endingRow === firstRow + 1;
+        } else {
+            return endingRow === firstRow - 1;
+        }
+    } else if(this.isKing(piece)){
+        return Math.abs(endingRow - firstRow) === 1;
+    } else {
+        return false;
+    }//new change
+  };
+
+  this.isTwoRows = function(firstRow, endingRow, piece){
+    if (!this.isKing(piece)){
+        if(this.isRed(piece)){
+            return endingRow === firstRow + 2;
+        } else {
+            return endingRow === firstRow - 2;
+        }
+    } else if(this.isKing(piece)){
+        return Math.abs(endingRow - firstRow) === 2;
+    }
+  };
+
+    this.isValidSquare = function(row, col){
+    return this.isEven(row) && this.isEven(col) || !this.isEven(row) && !this.isEven(col);
+  };
+
+   this.isEven = function(x){
+    return x % 2 === 0;
+  };
+
+
+}
+
+var gameRules = new Rules();
 
 function Game(){
  this.resetGame = function(){
@@ -65,15 +178,6 @@ function Game(){
     whoseTurn = 0;
     jumped = false;
   };//OK
-
-  this.switchTurn = function() {
-  if (whoseTurn === 1) {
-    whoseTurn = 0;
-  } else {
-    whoseTurn = 1;
-  }
-  jumped = false;
-  };
 }//OK
 
 
@@ -100,7 +204,7 @@ function Board(){
     for (var row = 0; row < checkerboard.length; row++){
       for (var col = 0; col < checkerboard[row].length; col++){
         var squareColor;
-        if (this.isValidSquare(row, col)){
+        if (gameRules.isValidSquare(row, col)){
           squareColor = "black";
         } else {
           squareColor = "red";
@@ -108,70 +212,6 @@ function Board(){
         new Square(squareColor, row, col);
       }
     }
-  };
-
-  this.findMiddleRow = function(firstRow, endingRow){
-    if (firstRow + 1 === endingRow - 1) {
-        middleRow = firstRow + 1;
-    } else if (firstRow - 1 === endingRow + 1){
-        middleRow = firstRow - 1;
-    } else {
-        return false;
-    }
-    return middleRow;
-  };//OK
-
-  this.findMiddleCol = function(firstCol, endingCol){
-    if (firstCol + 1 === endingCol - 1){
-        middleCol = firstCol + 1;
-    } else if (firstCol - 1 === endingCol + 1){
-        middleCol = firstCol - 1;
-    } else {
-        return false;
-    }
-    return middleCol;
-  };//OK
-
-  this.isAdjacentSpace = function(firstRow, firstCol, endingRow, endingCol){
-    return Math.abs(endingCol - firstCol) === 1;
-  };//OK
-
-  this.isTwoColumns = function(firstCol, endingCol){
-    return Math.abs(endingCol - firstCol) === 2;
-  };//OK
-
-  this.isNextRow = function(firstRow, endingRow, piece){
-    if (!checker.isKing(piece)){
-        if (checker.isRed(piece)){
-            return endingRow === firstRow + 1;
-        } else {
-            return endingRow === firstRow - 1;
-        }
-    } else if(checker.isKing(piece)){
-        return Math.abs(endingRow - firstRow) === 1;
-    } else {
-        return false;
-    }//new change
-  };
-
-  this.isTwoRows = function(firstRow, endingRow, piece){
-    if (!checker.isKing(piece)){
-        if(checker.isRed(piece)){
-            return endingRow === firstRow + 2;
-        } else {
-            return endingRow === firstRow - 2;
-        }
-    } else if(checker.isKing(piece)){
-        return Math.abs(endingRow - firstRow) === 2;
-    }
-  };
-
-    this.isValidSquare = function(row, col){
-    return this.isEven(row) && this.isEven(col) || !this.isEven(row) && !this.isEven(col);
-  };
-
-   this.isEven = function(x){
-    return x % 2 === 0;
   };
 }
 
@@ -187,19 +227,19 @@ function CheckerPiece(color, startSquare){
   this.startSquare = $(startSquare);
   var self = this;
   this.colorEl = (function(piece){
-    var newPiece = $('<img src="images/'+self.color+self.rank+'.jpg" class="'+self.color+self.rank+' new_piece"/>');
+    var newPiece = $('<img src="images/'+self.color+ 'Checker.jpg" class="'+self.color+ ' checker new_piece"/>');
     $(self.startSquare).append(newPiece);
     return newPiece;
   })(self);
 
   this.colorEl.on("click", function(event){
     event.stopPropagation();
-    console.log("hi");
+    $(".selected").removeClass("selected");
+    $(this).addClass("selected");
     // var pieceRow = $(this).parent().data("row");
     // var pieceCol = $(this).parent().data("col");
     // var clickedPiece;
-    //   $(".selected").removeClass("selected");
-    //   $(this).addClass("selected");
+
       // if ($(this).find(".blackKing").length !==0 && whoseTurn == 1){
       //       piece = "King";
       //   } else if ($(this).find(".redKing").length !==0 && whoseTurn == 0){
@@ -210,26 +250,31 @@ function CheckerPiece(color, startSquare){
       //       piece = "Checker";
       //   };
   });
+}//OK
 
-  this.relocatePiece = function(firstRow, firstCol, endingRow, endingCol, piece) {
-  piece = checkForPromotion(endingRow, endingCol, piece);
-  setSquare(endingRow, endingCol, piece);
-  setSquare(firstRow, firstCol, null);
-  activePieceCoords = { row: firstRow,
-                        col: firstCol };
 
-  return piece;
-  };
 
-  this.setActivePieceCoords = function(row, col) {
-  activePieceCoords["row"] = row;
-  activePieceCoords["col"] = col;
-  };
+function Square(color, row, col){
+  this.color = color;
+  this.el = (function(square){
+    var newSquare = $('<div class="square ' + square.color + '" data-col="' + col + '" data-row="' + row + '" id="' + row + '_' + col + '"></div>');
+    $("#checkerboard").append(newSquare);
+    return newSquare
+  })(this);
+
+  this.el.on("click", $.proxy( function(){
+    var pieceRow = $(".selected").parent().data("row");
+    var pieceCol = $(".selected").parent().data("col");
+    var currentPiece = $(".selected");
+    var endingPieceRow = $(this.el).data("row");
+    var endingPieceCol = $(this.el).data("col");
+    this.move(pieceRow, pieceCol, endingPieceRow, endingPieceCol, currentPiece);
+  }, this));
 
   this.move = function(firstRow, firstCol, endingRow, endingCol, piece){
-    if (isValidMove(firstRow, firstCol, endingRow, endingCol) || isValidJump(firstRow, firstCol, endingRow, endingCol, piece)){
-        if (isJump(endingRow, firstRow)){
-          piece = relocatePiece(firstRow, firstCol, endingRow, endingCol, piece);
+    if (gameRules.isValidMove(firstRow, firstCol, endingRow, endingCol, piece) || this.isValidJump(firstRow, firstCol, endingRow, endingCol, piece)){
+        if (gameRules.isJump(endingRow, firstRow)){
+          // piece = relocatePiece(firstRow, firstCol, endingRow, endingCol, piece);
             if (checkerboard[middleRow][middleCol] === "Checker" || checkerboard[middleRow][middleCol] === "King"){
                 countR--;
                 $("#end_turn").removeClass("hidden").css("background", "black").css("color", "white").css("margin-right", "400px").css("margin-left", "-540px").css("float", "right");
@@ -237,20 +282,21 @@ function CheckerPiece(color, startSquare){
                 countB--;
                 $("#end_turn").removeClass("hidden").css("background", "red").css("color", "black").css("margin-left", "400px").css("margin-right", "-540px").css("float", "left");
             }
-            setSquare(middleRow, middleCol, null);
-            jumped = true;
-            setActivePieceCoords(endingRow, endingCol);
-        } else if (!jumped) {
-            relocatePiece(firstRow, firstCol, endingRow, endingCol, piece);
-            setActivePieceCoords(-1, -1);
-            switchTurn();
+            gameRules.resetMiddleRowCol;
+            gameRules.jumped = true;
+            // setActivePieceCoords(endingRow, endingCol);
+        } else if (!gameRules.jumped) {
+            this.relocatePiece(firstRow, firstCol, endingRow, endingCol, piece);
+            $(".selected").removeClass("selected");
+            // setActivePieceCoords(-1, -1);
+            gameRules.switchTurn();
         }
 
-        if (countR === 0){
+        if ($(".red.checker").length === 0){
             blackWin();
             $("#blackScore > .score").html(blackScoreCount);
             resetGame();
-        } else if (countB === 0){
+        } else if ($(".black.checker").length === 0){
             redWin();
             $("#redScore > .score").html(redScoreCount);
              resetGame();
@@ -260,37 +306,42 @@ function CheckerPiece(color, startSquare){
     return true;
   };
 
+
+  this.relocatePiece = function(firstRow, firstCol, endingRow, endingCol, piece) {
+    this.checkForPromotion(endingRow, endingCol, piece);
+    this.setSquare(endingRow, endingCol, piece);
+  return piece;
+  };
+
+
+
+  this.isJumpToSquareOpen = function(firstRow, firstCol, endingRow, endingCol){
+    return (checkerboard[endingRow][endingCol] === null);
+  };
+
+  this.isOpponentOnJumpOverSquare = function(piece, firstRow, firstCol, endingRow, endingCol){
+    newBoard.findMiddleRow(firstRow, endingRow);
+    newBoard.findMiddleCol(firstCol, endingCol);
+    return newBoard.isNextRow(firstRow, middleRow, piece) && identifyOpponent.isOpponent(piece, middleRow, middleCol)
+  };//new change
+  // this.setActivePieceCoords = function(row, col) {
+  // activePieceCoords["row"] = row;
+  // activePieceCoords["col"] = col;
+  // };
+
+
+
   this.checkForPromotion = function(endingRow, endingCol, piece){
-    if (endingRow === 7 && this.isRed(piece)){
-        this.rank = "King";
-   } else if (endingRow === 0 && isBlack(piece)){
-        this.rank = "King";
+    if (endingRow === 7 && gameRules.isRed(piece)){
+        piece.addClass("king");
+   } else if (endingRow === 0 && gameRules.isBlack(piece)){
+        piece.addClass("king");
    } return piece;
   };//new change, not sure about this
 
   this.isPieceTurn = function(){
-    return ((this.isRed(piece) && whoseTurn === 0) || (this.isBlack(piece) && whoseTurn === 1));
+    return ((gameRules.isRed(piece) && whoseTurn === 0) || (gameRules.isBlack(piece) && whoseTurn === 1));
   };
-
-  this.isRed = function(piece){
-    return (this.color === "red");
-  };
-
-  this.isBlack = function(piece){
-    return (this.color === "black");
-  };
-
-  this.isKing = function(piece){
-    return (this.rank === "King");
-  };
-
-  this.isValidJump = function(firstRow, firstCol, endingRow, endingCol, piece){
-    return (newBoard.isJumpToSquareOpen(firstRow, firstCol, endingRow, endingCol) && this.isOpponentOnJumpOverSquare(piece, firstRow, firstCol, endingRow, endingCol) && newBoard.isValidSquare(endingRow, endingCol) && newBoard.isTwoRows(firstRow, endingRow, piece));
-  };//new change
-
-  this.isJump = function(endingRow, firstRow){
-    return (Math.abs(endingRow - firstRow) === 2);
-  };//OK
 
   this.isOpponent = function(piece, row, col){
     if (checkerboard[row][col] === null) {
@@ -305,26 +356,19 @@ function CheckerPiece(color, startSquare){
     };//new change
 
   this.setSquare = function(row, col, piece) {
-
-      // if (playableSquare){
-      //     var color = this.setColor(piece);
-      //     var rank = this.setRank(piece);
-      //     if (piece !== null){
-      //     $("#" + row + '_' + col).html('<img src="images/'+color+rank+'.jpg" style="width: 60px" class="'+color+rank+'"/>').children().css("margin", "5px");
-      //     checkerboard[row][col] = piece;
-      //     } else {
-      //         checkerboard[row][col] = null;
-      //         $("#" + row + '_' + col).html(null);
-      //     }
-      //     return piece;
-      // }
-    };
+    var targetSquare = $("#" + row + '_' + col);
+      if (gameRules.isValidSquare && piece !== null){
+          targetSquare.append(piece);
+      } else {
+          targetSquare.html(null);
+      }
+  };
 
 
   this.setColor = function(piece){
-    if (this.isRed(piece)){
+    if (gameRules.isRed(piece)){
         return "red";
-    } else if (this.isBlack(piece)){
+    } else if (gameRules.isBlack(piece)){
         return "black";
     } else {
         return null;
@@ -345,17 +389,7 @@ function CheckerPiece(color, startSquare){
    return checkerboard[row][col];
   };//OK
 
-  this.isValidMove = function(firstRow, firstCol, endingRow, endingCol){
-    var validSquare = new Square();
-    piece = checkerboard[firstRow][firstCol];
-    if (!(validSquare.isValidSquare(firstRow, firstCol) && validSquare.isValidSquare(endingRow, endingCol))){
-        return false;
-    } else if (newBoard.isNextRow(firstRow, endingRow, piece) && newBoard.isAdjacentSpace(firstRow, firstCol, endingRow, endingCol)){
-        return true;
-    } else {
-        return false;
-    }
-  };//new change
+ //new change
 
   //elt is DOM node; piece is type of piece, data type is string
   this.selectPiece = function(elt, piece) {
@@ -372,36 +406,10 @@ function CheckerPiece(color, startSquare){
       return true;
     }
   };
-}//OK
-
-
-
-function Square(color, row, col){
-  this.color = color;
-    this.el = (function(square){
-    var newSquare = $('<div class="piece ' + square.color + '" data-col="' + col + '" data-row="' + row + '" id="' + row + '_' + col + '"></div>');
-    $("#checkerboard").append(newSquare);
-    return newSquare
-  })(this);
-
-  this.el.on("click", function(){
-    console.log("hi");
-  });
-
-  this.isJumpToSquareOpen = function(firstRow, firstCol, endingRow, endingCol){
-    return (checkerboard[endingRow][endingCol] === null);
-  };
-
-  this.isOpponentOnJumpOverSquare = function(piece, firstRow, firstCol, endingRow, endingCol){
-    newBoard.findMiddleRow(firstRow, endingRow);
-    newBoard.findMiddleCol(firstCol, endingCol);
-    return newBoard.isNextRow(firstRow, middleRow, piece) && identifyOpponent.isOpponent(piece, middleRow, middleCol)
-  };//new change
 }
 
 $(document).ready(function() {
     var myGame = new Game();
-    var newBoard = new Board();
     newBoard.initializeBoard();
 
     // // $("#checkerboard div").on("click", function(event){
